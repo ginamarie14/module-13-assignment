cac288e06caa0e32b64278ed31bc52bd3cfddfcf
Gina Marie <ginamarie214@gmail.com>
2022-12-11 01:04:52 -0500

    latest

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
 
-router.use('/api', apiRoutes);
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
-
-module.exports = router;
